d64e1659fce0ee684a731ed93d2d4ff023ac0978
Nasim Talukdar <ntn.talukdar@gmail.com>
2023-11-21 13:07:08 +0600

    Create a hero section for Rapido

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
       },
       colors: {
         'primary': "#ECEEFF",
-        "coral-red": "#FF6452",
+        "coral-red": "#BF1A1C",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
         "white-400": "rgba(255, 255, 255, 0.80)"
@@ -32,8 +32,8 @@ export default {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
       },
       backgroundImage: {
-        'hero': "url('assets/images/collection-background.svg')",
-        'card': "url('assets/images/thumbnail-background.svg')",
+        'hero': "url('../assets/images/collection-background.svg')",
+        'card': "url('../assets/images/thumbnail-background.svg')",
       },
       screens: {
         "wide": "1440px"
